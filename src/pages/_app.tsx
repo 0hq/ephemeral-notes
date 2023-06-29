eac812e9e62664d2ad6c1c31d636240e1b1652fb
Will DePue <will@depue.net>
2023-06-29 02:59:39 -0700

    Version 1 done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { JetBrains_Mono } from 'next/font/google'
+
+const inter = JetBrains_Mono({ subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<main className={inter.className}>
+    <Component {...pageProps} />
+  </main>)
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,6 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
